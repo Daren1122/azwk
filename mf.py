0c9102f611630f772002b8dd5affd4d575b90f8c
Daren1122 <99273813+Daren1122@users.noreply.github.com>
2022-04-03 10:16:30 +0800

    Update mf.py

--- mf.py
+++ mf.py
@@ -5,44 +5,23 @@ from contextlib import redirect_stdout
 from azure.cli.core import get_default_cli
  
 # 1.检查配额以确定订阅类型，并确定要开的虚拟机数量
-# 初始化区域列表，共12个区域
+# 初始化区域列表，共8个区域
 # Azure for Students和即用即付订阅均不支持 South India 和 West India 区域
 locations = ['eastasia','japaneast','australiaeast','australiacentral','southindia','koreacentral','uaenorth','germanywestcentral']
  
-limit = "4" 
-# 默认每个区域的配额都相同，因此只需查询美国东部地区的配额
-# Azure for Students订阅每个区域的vCPU总数为6，
-# 标准FSv2系列vCPUs为4，标准FS系列vCPUs为4
-# 所以创建一个Standard_F4s_v2实例（占用4个vCPUs），
-# 一个Standard_F2s实例（占用2个vCPUs）
-if '6' in limit:
-    print("当前订阅为Azure for Students")
-    size1_name = "Standard_F4s_v2"
-    size1_abbreviation = "F4s_v2"
-    size1_count = 1
-    size2_name = "Standard_F2s"
-    size2_abbreviation = "F2s"
-    size2_count = 1
-    type = 0
- 
-# 即用即付订阅每个区域的vCPU总数为10，与标准FSv2系列的vCPUs相同
-# 因此创建一个Standard_F8s_v2实例（占用8个vCPUs），
-# 一个Standard_F2s_v2实例（占用2个vCPUs）
-elif '10' in limit:
-    print("当前订阅为即用即付")
-    size1_name = "Standard_F8s_v2"
-    size1_abbreviation = "F8s_v2"
-    size1_count = 1
-    size2_name = "Standard_F2s_v2"
-    size2_abbreviation = "F2s_v2"
-    size2_count = 1
-    type = 1
+# 捕获 get_default_cli().invoke 的标准输出
+f = io.StringIO()
+with redirect_stdout(f):
+    get_default_cli().invoke(['vm', 'list-usage', '--location', 'Australia Rast', '--query',
+                              '[?localName == \'Total Regional vCPUs\'].limit'])
+    limit = "4"
+
  
 # 免费试用订阅每个区域的vCPU总数为4，与标准FSv2系列的vCPUs相同
 # 因此创建1个Standard_F4s_v2实例（共占用4个vCPUs）
-elif '4' in limit:
-#   print("当前订阅为免费试用，每个区域的配额仅为4 vCPUs，建议升级后再用。"
-#         "若升级后仍看到本消息，请等待十分钟再运行脚本。")
+if '4' in limit:
+    print("当前订阅为免费试用，每个区域的配额仅为4 vCPUs，建议升级后再用。"
+          "若升级后仍看到本消息，请等待十分钟再运行脚本。")
 #   selection = input("输入Y继续运行，任意键退出")
 #   if selection != "Y" or "y":
 #       exit(0)
@@ -78,66 +57,59 @@ with open("./cloud-init.txt", "w") as f:
     f.write(f"  - {init}")
  
 # 4.批量创建虚拟机并运行挖矿脚本
-for x in ['1','2']:
-    print("第{}次运行".format(x))
-    for location in locations:
-         # Azure for Students订阅不支持 norwayeast 区域
-        if location == "norwayeast" and type == 0:
-            continue
-     
-        # westcentralus 区域不支持 FSv2 系列，
-        # Azure for Students订阅不支持 F/FS 系列
-        if location == "westcentralus" and type == 0:
-            size1_name = "Standard_D4ds_v4"
-            size1_abbreviation = "D4ds_v4"
-            size2_name = "Standard_D2s_v4"
-            size2_abbreviation = "D2s_v4"
-        if location == "westcentralus" and type == 1:
-            size1_name = "Standard_F8s"
-            size1_abbreviation = "F8s"
-            size2_name = "Standard_F2s"
-            size2_abbreviation = "F2s"
-        if location == "westcentralus" and type == 2:
-            size1_name = "Standard_F4s"
-            size1_abbreviation = "F4s"
-     
+for location in locations:
+    for i in range(5, -1, -1):
+        print("\r等待{}秒,创建下一个".format(i), end="", flush=True)
+        time.sleep(1)
+     # Azure for Students订阅不支持 norwayeast 区域
+    if location == "norwayeast" and type == 0:
+        continue
+ 
+    # westcentralus 区域不支持 FSv2 系列，
+    # Azure for Students订阅不支持 F/FS 系列
+    if location == "westcentralus" and type == 0:
+        size1_name = "Standard_D4ds_v4"
+        size1_abbreviation = "D4ds_v4"
+        size2_name = "Standard_D2s_v4"
+        size2_abbreviation = "D2s_v4"
+    if location == "westcentralus" and type == 1:
+        size1_name = "Standard_F8s"
+        size1_abbreviation = "F8s"
+        size2_name = "Standard_F2s"
+        size2_abbreviation = "F2s"
+    if location == "westcentralus" and type == 2:
+        size1_name = "Standard_F4s"
+        size1_abbreviation = "F4s"
+ 
+    count = 0
+    for a in range(0, size1_count):
+        count += 1
+        ts = time.time()
+        print("正在 " + str(location) + " 区域创建第 " + str(count)
+              + f" 个 {size1_name} 实例，共 " + str(size1_count) + " 个")
+        get_default_cli().invoke(
+            ['vm', 'create', '--resource-group', 'myResourceGroup', '--name',
+             f'{location}-{size1_abbreviation}-{int(ts)}', '--image', 'UbuntuLTS',
+             '--size', f'{size1_name}', '--location', f'{location}', '--admin-username',
+             'azureuser', '--admin-password', '6uPF5Cofvyjcew9', '--custom-data',
+             'cloud-init.txt', "--no-wait"])
+    if type != 2:
         count = 0
-        for a in range(0, size1_count):
+        for a in range(0, size2_count):
             count += 1
-            ts = time.time()
             print("正在 " + str(location) + " 区域创建第 " + str(count)
-                  + f" 个 {size1_name} 实例，共 " + str(size1_count) + " 个")
+                  + f" 个 {size2_name} 实例，共 " + str(size2_count) + " 个")
             get_default_cli().invoke(
                 ['vm', 'create', '--resource-group', 'myResourceGroup', '--name',
-                 f'{location}-{size1_abbreviation}-{int(ts)}', '--image', 'UbuntuLTS',
-                 '--size', f'{size1_name}', '--location', f'{location}', '--admin-username',
+                 f'{location}-{size2_abbreviation}-{int(ts)}', '--image', 'UbuntuLTS',
+                 '--size', f'{size2_name}', '--location', f'{location}', '--admin-username',
                  'azureuser', '--admin-password', '6uPF5Cofvyjcew9', '--custom-data',
                  'cloud-init.txt', "--no-wait"])
-        if type != 2:
-            count = 0
-            for a in range(0, size2_count):
-                count += 1
-                ts = time.time()
-                print("正在 " + str(location) + " 区域创建第 " + str(count)
-                      + f" 个 {size2_name} 实例，共 " + str(size2_count) + " 个")
-                get_default_cli().invoke(
-                    ['vm', 'create', '--resource-group', 'myResourceGroup', '--name',
-                     f'{location}-{size2_abbreviation}-{int(ts)}', '--image', 'UbuntuLTS',
-                     '--size', f'{size2_name}', '--location', f'{location}', '--admin-username',
-                     'azureuser', '--admin-password', '6uPF5Cofvyjcew9', '--custom-data',
-                     'cloud-init.txt', "--no-wait"])
-    if '1' in x: 
-        print("\n------------------------------------------------------------------------------\n")
-        print("以下是已创建的虚拟机列表：")
-        get_default_cli().invoke(['vm', 'list', '--query', '[*].name'])
-        print("\n\n-----------------------------------------------------------------------------\n")
-        for i in range(10, -1, -1):
-            print("\r稍后将运行第二遍脚本，还需等待{}秒".format(i), end="", flush=True)
-            time.sleep(1)
+   
 # 5.信息汇总
 # 获取所有vm的名字
 print("\n------------------------------------------------------------------------------\n")
-print("大功告成！在31个区域创建虚拟机的命令已成功执行")
+print("大功告成！在8个区域创建虚拟机的命令已成功执行")
 for i in range(60, -1, -1):
     print("\r正在等待Azure生成统计信息，还需等待{}秒".format(i), end="", flush=True)
     time.sleep(1)
@@ -149,5 +121,6 @@ print("\n\n---------------------------------------------------------------------
  
  
 # 如果想删除脚本创建的所有资源，取消注释以下语句
-# get_default_cli().invoke(['group', 'delete', '--name', 'myResourceGroup', '--no-wait', '--yes'])
+# get_default_cli().invoke(['group', 'delete', '--name', 'myResourceGroup',
+# '--no-wait', '--yes'])
 # print("删除资源组成功")
